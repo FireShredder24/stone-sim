780c033df1f7bd8f3e00adbbbd4c3848da0b51d6
John Nguyen <knightofthealtar64@gmail.com>
2024-05-17 23:07:47 -0700

    Refactor to add conservation of thermal energy
    Need boiling point as function of pressure though

--- copv_press.py
+++ copv_press.py
@@ -20,18 +20,22 @@ R_inlb = R_btu * 9338.0318994166 # in-lbf / (lbmol * R)
 def vapor_pressure(h_vap, M, boil, T):
     return 14.7 * np.exp(-h_vap * M / R_btu * (1 / T - 1 / boil))
 
+def boiling_point(h_vap, M, press_curr, T_boil):
+    return h_vap * M / (log(press_curr / 14.7) * R_btu) + T_boil
+
 # System definitions
 
 copv_vol = 416 # cu. in.
 ox_tank_vol = 980 # cu. in.
 fuel_tank_vol = 814 # cu. in.
+radius = 3.75 # in
 
 ox_dr_press = 600 + 14.7 # psia, ox side dome regulator pressure setting
 fuel_dr_press = 565 + 14.7 # psia, fuel side dome regulator pressure setting
 
 T_amb = 530 # R, ambient temperature
 
-copv_press = 3000
+copv_press = 3500
 copv_T = T_amb # R, initial COPV temperature
 
 ox_ul_press = 600 + 14.7 # psia, initial ox tank ullage gas pressure
@@ -55,10 +59,14 @@ ox_name = "Liquid Oxygen"
 ox_M = 32 # lbm / lbmol, molar mass of oxidizer
 ox_T_boil = 162.4 # R, oxidizer boiling point
 ox_T = 200 # R, oxidizer initial temperature
+ox_T_l = 162.4 # R, liquid oxidizer temperature
 ox_rho_l = 71.16 / 12**3 # lb / in^3
+ox_mass_l = ox_rho_l * (ox_tank_vol - ox_ul_vol) # lbm, mass of liquid oxidizer
 ox_heat_vap = 91.7 # BTU / lbm
-ox_vap_press = vapor_pressure(ox_heat_vap, ox_M, ox_T_boil, ox_T) # psia, ox vapor partial pressure
-ox_total_press += ox_vap_press # psia, ox tank total pressure
+ox_htc = 1e-4 # Convective heat transfer coefficient b/w liquid oxidizer and ullage gas
+ox_max_vap_press = vapor_pressure(ox_heat_vap, ox_M, ox_T_boil, ox_T) # psia, ox vapor partial pressure
+ox_vap_press = ox_max_vap_press
+ox_total_press += ox_max_vap_press # psia, ox tank total pressure
 ox_c_l = 0.219 # BTU / lbm / R
 ox_cp = 0.22 # BTU / lbm / R, specific heat at constant pressure
 ox_cv = 0.16 # BTU / lbm / R, specific heat at constant volume
@@ -68,12 +76,16 @@ ox_cv = 0.16 # BTU / lbm / R, specific heat at constant volume
 fuel_name = "Kerosene"
 
 fuel_M = 177 # lbm / lbmol, molar mass of fuel
-fuel_T = T_amb # R, fuel initial temperature
+fuel_T = T_amb # R, fuel tank ullage gas initial temperature
+fuel_T_l = T_amb # R, liquid fuel initial temperature
 fuel_T_boil = 770 # R, fuel boiling point
 fuel_rho_l = 51.1 / 12**3 # lb / in^3, liquid phase density
+fuel_mass_l = fuel_rho_l * (fuel_tank_vol - fuel_ul_vol) # lbm, mass of liquid fuel
 fuel_heat_vap = 108 # BTU / lbm, heat of vaporization
-fuel_vap_press = vapor_pressure(fuel_heat_vap, fuel_M, fuel_T_boil, fuel_T) # psia, fuel vapor partial pressure
-fuel_total_press += fuel_vap_press # psia, fuel tank total pressure
+fuel_htc = ox_htc # Convective heat transfer coefficient b/w liquid oxidizer and ullage gas
+fuel_max_vap_press = vapor_pressure(fuel_heat_vap, fuel_M, fuel_T_boil, fuel_T) # psia, fuel vapor partial pressure
+fuel_vap_press = fuel_max_vap_press
+fuel_total_press += fuel_max_vap_press # psia, fuel tank total pressure
 fuel_c_l = 0.48 # BTU / lbm / R
 # values for kerosene unknown.  Using same as nitrogen since kerosene unlikely to vaporize sigificantly
 fuel_cp = 0.25 # BTU / lbm / R, specific heat at constant pressure
@@ -90,10 +102,10 @@ ul_heat_vap = 86 # BTU / lbm, heat of vaporization
 ul_cp = 0.25 # BTU / lbm / R, specific heat at constant pressure
 ul_cv = 0.18 # BTU / lbm / R, specific heat at constant volume
 
-ox_vapor_mass = ox_M * ox_vap_press * ox_ul_vol / R_inlb / ox_T
+ox_vapor_mass = ox_M * ox_max_vap_press * ox_ul_vol / R_inlb / ox_T
 ox_ul_mass = ul_M * ox_ul_press * ox_ul_vol / R_inlb / ox_T
 
-fuel_vapor_mass = fuel_M * fuel_vap_press * fuel_ul_vol / R_inlb / fuel_T
+fuel_vapor_mass = fuel_M * fuel_max_vap_press * fuel_ul_vol / R_inlb / fuel_T
 fuel_ul_mass = ul_M * fuel_ul_press * fuel_ul_vol / R_inlb / fuel_T
 
 copv_ul_mass = ul_M * copv_press * copv_vol / R_inlb / copv_T
@@ -116,8 +128,10 @@ temp_graph = graph(title="Temperature", fast=False, xtitle="s", ytitle="R")
 copv_T_curve = gcurve(graph=temp_graph, label="COPV", color=color.green)
 ox_T_curve = gcurve(graph=temp_graph, label="OX", color=color.blue)
 ox_dr_T_curve = gcurve(graph=temp_graph, label="OX DR", color=color.purple)
+ox_T_l_curve = gcurve(graph=temp_graph, label="OX LIQ", color=color.cyan)
 fuel_T_curve = gcurve(graph=temp_graph, label="FUEL", color=color.red)
 fuel_dr_T_curve = gcurve(graph=temp_graph, label="FUEL DR", color=color.orange)
+fuel_T_l_curve = gcurve(graph=temp_graph, label="FUEL LIQ", color=color.black)
 
 volume_graph = graph(title="Volume", fast=False, xtitle="s", ytitle="in^3")
 ox_vol_curve = gcurve(graph=volume_graph, label="OX", color=color.blue)
@@ -131,18 +145,63 @@ fuel_ul_mass_curve = gcurve(graph=mass_graph, label="FUEL UL", color=color.red)
 fuel_vap_mass_curve = gcurve(graph=mass_graph, label="FUEL VAP", color=color.orange)
 
 t = 0
-dt = 0.025
+dt = 0.1
+
+copv_underpress = None
 
 print("Begin simulation")
 while ox_ul_vol <= ox_tank_vol and fuel_ul_vol <= fuel_tank_vol and t < 30:
     # Calculating vapor pressures of liquid propellants.
-    # We assume that the relatively violent pressurization will cause instant saturation.
+    # This is no longer the pressure added to the tank pressure,
+    # but a reference value with which to calculate the rate of evaporation.
     # Note that the temperature here corresponds to the temperature of the gas above liquid.
-    # This is added to the total pressure of the tank
-    ox_vap_press = vapor_pressure(ox_heat_vap, ox_M, ox_T_boil, ox_T)
-    fuel_vap_press = vapor_pressure(fuel_heat_vap, fuel_M, fuel_T_boil, fuel_T)
+    ox_max_vap_press = vapor_pressure(ox_heat_vap, ox_M, ox_T_boil, ox_T)
+    fuel_max_vap_press = vapor_pressure(fuel_heat_vap, fuel_M, fuel_T_boil, fuel_T)
+    ox_boiling_point = boiling_point(ox_heat_vap, ox_M, ox_total_press, ox_T_boil)
+    fuel_boiling_point = boiling_point(fuel_heat_vap, fuel_M, fuel_total_press, fuel_T_boil)
+
+    # BEGIN VAPORIZATION
+
+    # Convective heat transfer between liquid propellant and ullage gas
+    ox_heat_transfer = ox_htc * pi * radius**2 * (ox_T - ox_T_l) * dt
+    fuel_heat_transfer = fuel_htc * pi * radius**2 * (fuel_T - fuel_T_l) * dt
+
+    # Change in temperature of the liquid propellant
+    ox_T_delta = ox_heat_transfer / (ox_mass_l * ox_c_l)
+    fuel_T_delta = fuel_heat_transfer / (fuel_mass_l * fuel_c_l)
+
+    # If the temperature would rise higher than the boiling point, the residual energy goes to vaporization
+    if ox_T_l + ox_T_delta > ox_T_boil:
+        ox_residual_heat = (ox_T_delta + ox_T_l - ox_T_boil) * ox_mass_l * ox_c_l
+        ox_new_vapor_mass = ox_residual_heat / ox_heat_vap
+        ox_mass_l -= ox_new_vapor_mass
+        ox_vapor_mass += ox_new_vapor_mass
+        ox_T_l = ox_T_boil
+    else:
+        ox_T_l += ox_T_delta
+        ox_new_vapor_mass = 0
+
+    if fuel_T_l + fuel_T_delta > fuel_T_boil:
+        fuel_residual_heat = (fuel_T_delta + fuel_T_l - fuel_T_boil) * fuel_mass_l * fuel_c_l
+        fuel_new_vapor_mass = fuel_residual_heat / fuel_heat_vap
+        fuel_mass_l -= fuel_new_vapor_mass
+        fuel_vapor_mass += fuel_new_vapor_mass
+        fuel_T_l = fuel_T_boil
+    else:
+        fuel_T_l += fuel_T_delta
+        fuel_new_vapor_mass = 0
+
+    # Now remove that amount of heat from the ullage gas
+    ox_T = (((ox_vapor_mass-ox_new_vapor_mass)*ox_cv*ox_T + ox_ul_mass*ul_cv*ox_T + ox_new_vapor_mass*ox_cv*ox_T_boil) - ox_heat_transfer) \
+           / (ox_vapor_mass*ox_cv + ox_ul_mass*ul_cv)
+    fuel_T = (((fuel_vapor_mass-fuel_new_vapor_mass)*fuel_cv*fuel_T + fuel_ul_mass*ul_cv*fuel_T + fuel_new_vapor_mass*fuel_cv*fuel_T_boil) - fuel_heat_transfer) \
+           / (fuel_vapor_mass * fuel_cv + fuel_ul_mass*ul_cv)
+
+    # VAPORIZATION DONE
+    # BEGIN DOME REGULATOR PRESSURIZATION
 
     # First we need to calculate the temperature at the dome regulator outlets
+    # Using law of adiabatic expansion
     ox_dr_T = copv_T * (ox_dr_press / copv_press) ** (1-ul_cv/ul_cp)
     fuel_dr_T = copv_T * (fuel_dr_press / copv_press) ** (1-ul_cv/ul_cp)
 
@@ -157,25 +216,32 @@ while ox_ul_vol <= ox_tank_vol and fuel_ul_vol <= fuel_tank_vol and t < 30:
         fuel_new_mass = 0
 
     # Then, due to pressure drop over the pipework, the temperature of the gas entering the tanks will be lower
+    # Using adiabatic expansion law, similar to dome reg outlet temperature
     ox_dr_T = ox_dr_T * ((ox_dr_press - ox_press_drop) / ox_dr_press) ** (1-ul_cv/ul_cp)
     fuel_dr_T = fuel_dr_T * ((fuel_dr_press - fuel_press_drop) / fuel_dr_press) ** (1-ul_cv/ul_cp)
 
     # Then we take the existing mass within the tanks and determine the final temperature
+    # Using Q = mc(t2-t1)
+    ox_T = (ox_vapor_mass*ox_cv*ox_T + ox_ul_mass*ul_cv*ox_T + ox_new_mass*ul_cv*ox_dr_T) \
+           / (ox_vapor_mass*ox_cv + (ox_ul_mass + ox_new_mass)*ul_cv)
 
-    ox_T = (ox_vapor_mass*ox_cv*ox_T + ox_ul_mass*ul_cv*ox_T + ox_new_mass*ul_cv*ox_dr_T) / (ox_vapor_mass*ox_cv + (ox_ul_mass + ox_new_mass)*ul_cv)
+    fuel_T = (fuel_vapor_mass*fuel_cv*fuel_T + fuel_ul_mass*ul_cv*fuel_T + fuel_new_mass*ul_cv*fuel_dr_T) \
+             / (fuel_vapor_mass*fuel_cv + (fuel_ul_mass + fuel_new_mass)*ul_cv)
 
-    fuel_T = (fuel_vapor_mass*fuel_cv*fuel_T + fuel_ul_mass*ul_cv*fuel_T + fuel_new_mass*ul_cv*fuel_dr_T) / (fuel_vapor_mass*fuel_cv + (fuel_ul_mass + fuel_new_mass)*ul_cv)
-
-    # Then we find final pressure
+    # Then we find final pressure using Ideal Gas Law
     ox_ul_press = (ox_ul_mass + ox_new_mass) / ul_M * R_inlb * ox_T / ox_ul_vol
+    ox_vap_press = ox_vapor_mass / ox_M * R_inlb * ox_T / ox_ul_vol
     ox_total_press = ox_ul_press + ox_vap_press
 
     fuel_ul_press = (fuel_ul_mass + fuel_new_mass) / ul_M * R_inlb * fuel_T / fuel_ul_vol
+    fuel_vap_press = fuel_vapor_mass / fuel_M * R_inlb * fuel_T / fuel_ul_vol
     fuel_total_press = fuel_ul_press + fuel_vap_press
 
-    # And increase the ullage volume available
-    ox_ul_vol += ox_vol_rate * dt
-    fuel_ul_vol += fuel_vol_rate * dt
+    # And increase the ullage volume available, and reduce the mass of liquid propellant
+    ox_mass_l -= ox_vol_rate * ox_rho_l * dt
+    fuel_mass_l -= fuel_vol_rate * fuel_rho_l * dt
+    ox_ul_vol = ox_tank_vol - ox_mass_l / ox_rho_l
+    fuel_ul_vol = fuel_tank_vol - fuel_mass_l / fuel_rho_l
 
     # And now update the COPV pressure and temperature
     copv_ul_mass -= ox_new_mass + fuel_new_mass
@@ -183,16 +249,14 @@ while ox_ul_vol <= ox_tank_vol and fuel_ul_vol <= fuel_tank_vol and t < 30:
     copv_press = copv_ul_mass / ul_M * R_inlb * copv_T / copv_vol
     copv_T = copv_T*(copv_press / copv_press_old) ** (1-ul_cv/ul_cp)
 
-    # Update vapor and ullage mass
-
-    ox_vapor_mass = ox_M * ox_vap_press * ox_ul_vol / R_inlb / ox_T
+    # Update ullage mass
     ox_ul_mass += ox_new_mass
-
-    fuel_vapor_mass = fuel_M * fuel_vap_press * fuel_ul_vol / R_inlb / fuel_T
     fuel_ul_mass += fuel_new_mass
 
-    # Printing for posterity
-    print(f"t={t:.2f}, copv: {copv_press:.0f} psia & {copv_T:.0f} R, ox at {ox_total_press:.0f} psia & {ox_T:.0f} R, fuel at {fuel_total_press:.0f} psia & {fuel_T:.0f} R")
+    # Save event variables if they occured
+    if not copv_underpress and copv_press < max(ox_dr_press, fuel_dr_press):
+        copv_underpress = t
+
 
     # Graphing
     copv_press_curve.plot(t, copv_press)
@@ -202,18 +266,24 @@ while ox_ul_vol <= ox_tank_vol and fuel_ul_vol <= fuel_tank_vol and t < 30:
     ox_vap_press_curve.plot(t, ox_vap_press)
     ox_T_curve.plot(t, ox_T)
     ox_dr_T_curve.plot(t, ox_dr_T)
+    ox_T_l_curve.plot(t, ox_T_l)
     ox_ul_mass_curve.plot(t, ox_ul_mass + ox_new_mass)
     ox_vap_mass_curve.plot(t, ox_vapor_mass)
     fuel_press_curve.plot(t, fuel_total_press)
     fuel_vap_press_curve.plot(t, fuel_vap_press)
     fuel_T_curve.plot(t, fuel_T)
     fuel_dr_T_curve.plot(t, fuel_dr_T)
+    fuel_T_l_curve.plot(t, fuel_T_l)
     fuel_ul_mass_curve.plot(t, fuel_ul_mass + fuel_new_mass)
     fuel_vap_mass_curve.plot(t, fuel_vapor_mass)
     ox_vol_curve.plot(t, ox_ul_vol)
     fuel_vol_curve.plot(t, fuel_ul_vol)
 
-
-
     # and of course increment time
     t += dt
+
+
+# Final report
+print("End simulation")
+if copv_underpress:
+    print(f"COPV fell below highest DR press at T+{copv_underpress:.2f} seconds")
